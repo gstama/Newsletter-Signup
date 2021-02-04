7ca175fb58e7efe5fdb3fa7056ef7422cbbb8980
gstama <gstama@outlook.com>
2021-02-04 19:46:20 +0000

    signup.html setup with bootstrap, css added, app posts sign up data

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', (req, res, next) => {
+  console.log(req.body.inputFirstName);
+  console.log(req.body.inputLastName);
+  console.log(req.body.inputEmail);
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
